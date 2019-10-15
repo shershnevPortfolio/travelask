7fc426d462a5174b7609669ec5cac0efeea94e08
rayne8k <shershnev.danil2000@gmail.com>
2019-10-16 01:49:42 +0300

    done slider

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,4 @@
 var reviwesArticleMargin = 8;
- var left = 0;
  var reviwesArticles =  document.getElementsByClassName('reviews__article');
 
  function setArticlesMargins() {
@@ -24,20 +23,38 @@ var reviwesArticleMargin = 8;
 } 
 
 function slideArticles() {
+	 let activeCarouselSwitch = document.getElementsByClassName('active carousel__switch');
+	 /*var carouselSwitch = document.getElementsByClassName('carousel__switch');*/
+	 /*let carouselSwitchContainer = document.getElementById('carousel__switch-container');*/
+	 /*carouselSwitchContainer.lastChild.onclick = function() {
+	 }*/
     if (this.classList.contains('active')) {
         return 0;
     }
-   let activeCarouselSwitch = document.getElementsByClassName('active carousel__switch');
+
+
+    if (this.id.slice(this.id.lastIndexOf('-') + 1,this.id.length) == 1) {
+   	  document.getElementsByClassName('reviews__section')[0].style.left = 0 + 'px';
+   	  activeCarouselSwitch[0].classList.toggle('active'); 
+   	  this.classList.toggle('active');
+    }
+    else {
+  
    let numbersOfSwitchesBetweenActiveAndThis = this.id.slice(this.id.lastIndexOf('-') + 1,this.id.length) -
      activeCarouselSwitch[0].id.slice(activeCarouselSwitch[0].id.lastIndexOf('-') + 1,activeCarouselSwitch[0].id.length);
-     alert(numbersOfSwitchesBetweenActiveAndThis);
-   left -= (reviwesArticles[0].offsetWidth + reviwesArticleMargin)*3 ;
+   left = (reviwesArticles[0].offsetWidth + reviwesArticleMargin)*3 ;
    var reviewsContainer = document.getElementsByClassName('reviews');
-   document.getElementsByClassName('reviews__section')[0].style.left = left * numbersOfSwitchesBetweenActiveAndThis  + 'px';  
+   var currentLeft = getComputedStyle(document.getElementsByClassName('reviews__section')[0]).getPropertyValue('left');
+   document.getElementsByClassName('reviews__section')[0].style.left =  left * numbersOfSwitchesBetweenActiveAndThis * -1 + 
+     Number(currentLeft.slice(0,currentLeft.length - 2)) + 'px';
+   alert(currentLeft);
+    activeCarouselSwitch[0].classList.toggle('active');
+   this.classList.toggle('active'); 
+  }
+ 
    
            
-   activeCarouselSwitch[0].classList.toggle('active');
-   this.classList.toggle('active'); 
+   
   
 }
 
@@ -45,3 +62,4 @@ function slideArticles() {
 
 
 window.addEventListener('load', setArticlesMargins);
+		
